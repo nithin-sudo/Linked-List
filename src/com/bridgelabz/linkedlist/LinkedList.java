00ad2c68422bfdeff878a530e1bc1be52ed1c848
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-07-02 13:03:05 +0530

    Added: addElement method adding element by creating linked list.

--- src/com/bridgelabz/linkedlist/LinkedList.java
+++ src/com/bridgelabz/linkedlist/LinkedList.java
@@ -1,5 +1,18 @@
 package com.bridgelabz.linkedlist;
 
-public class LinkedList {
-    
+public class LinkedList<T> {
+    Node head;
+    /**
+     * adding elements at the back of each node.
+     *
+     * @param data
+     * @param <T>
+     */
+    public <T extends Comparable> void addElement(T data) {
+        Node newNode = new Node();
+        newNode.data = data;
+        newNode.next = null;
+        newNode.next = head;
+        head = newNode;
+    }
 }
--- src/com/bridgelabz/linkedlist/Runner.java
+++ src/com/bridgelabz/linkedlist/Runner.java
@@ -2,6 +2,10 @@ package com.bridgelabz.linkedlist;
 
 public class Runner {
     public static void main(String[] args) {
-
+        LinkedList<Integer> list = new <Integer>LinkedList();
+        list.addElement(70);
+        list.addElement(36);
+        list.addElement(50);
+        list.addElement(30);
     }
 }
